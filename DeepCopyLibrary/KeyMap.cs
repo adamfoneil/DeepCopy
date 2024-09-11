8bc9bca933f140b3d303af320426859fe2c13563
Adam O'Neil <adamosoftware@gmail.com>
2024-09-11 07:59:55 -0400

    named tuple args

--- DeepCopyLibrary/KeyMap.cs
+++ DeepCopyLibrary/KeyMap.cs
@@ -2,7 +2,7 @@
 
 public abstract class KeyMap<TKey> where TKey : notnull
 {
-	private Dictionary<(string, TKey), TKey> _keyMap = [];
+	private Dictionary<(string StepName, TKey SourceKey), TKey> _keyMap = [];
 
 	protected abstract Task LoadAsync();
 }
