3bdad74430309949c1d12f1e8d6613afaa0e8b32
Adam O'Neil <adamosoftware@gmail.com>
2024-09-12 08:09:03 -0400

    indexer access on key map repository

--- DeepCopyLibrary/IKeyMapRepository.cs
+++ DeepCopyLibrary/IKeyMapRepository.cs
@@ -6,4 +6,5 @@ public interface IKeyMapRepository<TKey> where TKey : notnull
 {
 	bool ContainsKey(string stepName, TKey key);
 	Task AddAsync(string stepName, TKey sourceKey, TKey targetKey, IDbTransaction transaction);
+	TKey this[string stepName, TKey key] { get; }
 }
