ca5ff5b4d9f02d628a51fcbdf57764a98c1f78fb
Adam O'Neil <adamosoftware@gmail.com>
2024-09-13 07:12:29 -0400

    log tweaks

--- DeepCopyLibrary/IErrorRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DeepCopyLibrary;
-
-public interface IErrorRepository
-{
-	Task WhenInsertingAsync<TEntity>(string stepName, Exception exception, TEntity sourceRow, TEntity newRow);
-	Task WhenQueryingAsync(string stepName, Exception exception);
-	Task WhenLoopingAsync(string stepName, Exception exception);
-}
--- DeepCopyLibrary/RemoteDeepCopy.cs
+++ DeepCopyLibrary/RemoteDeepCopy.cs
@@ -4,21 +4,29 @@ using System.Diagnostics;
 
 namespace DeepCopyLibrary;
 
+public enum ErrorLocation
+{
+	Querying,
+	Looping,
+	Creating,
+	Inserting
+}
+
 /// <summary>
 /// Performs a multi-step copy operation between two connections, allowing for stopping and resuming.
 /// </summary>
 public abstract class RemoteDeepCopy<TKey, TInputParams, TOutput>(
-	IKeyMapRepository<TKey> keyMapRepository,
-	IErrorRepository errorRepository,
+	IKeyMapRepository<TKey> keyMapRepository,	
 	IMetricsRepository metricsRepository,
 	ILogger<RemoteDeepCopy<TKey, TInputParams, TOutput>> logger)
 	where TInputParams : new()
 	where TKey : notnull
 {
 	private readonly IKeyMapRepository<TKey> _keyMap = keyMapRepository;
-	private readonly IErrorRepository _errors = errorRepository;
 	private readonly IMetricsRepository _metrics = metricsRepository;
 	private readonly ILogger<RemoteDeepCopy<TKey, TInputParams, TOutput>> _logger = logger;
+
+	public abstract int MaxErrors { get; }
 
 	/// <summary>
 	/// override this to invoke your various Step classes (using Step.ExecuteAsync)
@@ -30,14 +38,12 @@ public abstract class RemoteDeepCopy<TKey, TInputParams, TOutput>(
 	/// </summary>   
 	protected abstract class Step<TEntity>(
 		IKeyMapRepository<TKey> keyMap,
-		IErrorRepository errorRepository,
 		IMetricsRepository metricsRepository,
 		ILogger<Step<TEntity>> logger) where TEntity : new()
 	{
 		protected readonly IKeyMapRepository<TKey> KeyMap = keyMap;		
 		protected readonly ILogger<Step<TEntity>> Logger = logger;
-
-		protected readonly IErrorRepository _errors = errorRepository;
+		
 		private readonly IMetricsRepository _metrics = metricsRepository;
 
 		protected abstract string Name { get; }
@@ -50,18 +56,23 @@ public abstract class RemoteDeepCopy<TKey, TInputParams, TOutput>(
 		{
 			if (cancellationToken.IsCancellationRequested) return;
 
+			using var logScope = Logger.BeginScope("Step {StepName}, input params {@parameters}", Name, parameters);
+
+			const string logTemplate = "Error in {location}, source key {sourceKey}";
+
 			var sw = Stopwatch.StartNew();			
 			int successRows = 0;
-			int errorRows = 0;
+			int createErrors = 0;
+			int insertErrors = 0;			
 			int skippedRows = 0;
 
 			try
 			{
-				Logger.LogDebug("Querying source rows for step {StepName}", Name);
+				Logger.LogDebug("Querying...");
 				var sourceRows = await QuerySourceRowsAsync(sourceConnection, parameters);
 
 				try
-				{
+				{ 
 					foreach (var sourceRow in sourceRows)
 					{
 						if (cancellationToken.IsCancellationRequested) break;
@@ -70,38 +81,43 @@ public abstract class RemoteDeepCopy<TKey, TInputParams, TOutput>(
 						if (KeyMap.ContainsKey(Name, sourceKey))
 						{
 							skippedRows++;
-							Logger.LogDebug("Skipping row with key {Key} for step {StepName}", sourceKey, Name);
+							Logger.LogDebug("Skipping row with key {Key}", sourceKey);
 							continue;
 						}
 
-						var newRow = CreateNewRow(parameters, sourceRow);
 						try
 						{
-							var newKey = await InsertNewRowAsync(destConnection, newRow, parameters);
-							successRows++;
-							await KeyMap.AddAsync(Name, sourceKey, newKey);							
+							var newRow = CreateNewRow(parameters, sourceRow);
+
+							try
+							{
+								var newKey = await InsertNewRowAsync(destConnection, newRow, parameters);
+								successRows++;
+								await KeyMap.AddAsync(Name, sourceKey, newKey);
+							}
+							catch (Exception exc)
+							{
+								insertErrors++;
+								Logger.LogError(exc, logTemplate, ErrorLocation.Inserting, sourceKey);
+							}
 						}
 						catch (Exception exc)
 						{
-							errorRows++;
-							Logger.LogError(exc, "Error inserting row for step {StepName} source key {key}", Name, sourceKey);
-							await _errors.WhenInsertingAsync(Name, exc, sourceRow, newRow);							
+							createErrors++;
+							Logger.LogError(exc, logTemplate, ErrorLocation.Creating, sourceKey);
 						}
+						
 					}
-				}
-				catch (Exception exc)
-				{
-					await _errors.WhenLoopingAsync(Name, exc);
 				}
 				finally
 				{
 					sw.Stop();
-					await _metrics.LogAsync(Name, successRows, errorRows, skippedRows, sw.Elapsed);
+					await _metrics.LogAsync(Name, successRows, insertErrors, skippedRows, sw.Elapsed);
 				}
 			}
 			catch (Exception exc)
 			{
-				await _errors.WhenQueryingAsync(Name, exc);
+				Logger.LogError(exc, logTemplate, ErrorLocation.Querying, default);
 			}
 		}
 	}
